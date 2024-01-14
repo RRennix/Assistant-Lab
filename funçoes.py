fad53f25579501a961b1b2cad200091de70d1030
Renix <renanavelarr2@gmail.com>
2024-01-13 21:18:47 -0300

    General rework

--- funçoes.py
+++ funçoes.py
@@ -1,24 +1,27 @@
 # 1.We need to import this modules for the code can understand what do with the variables that we will pass in the main.py
 import os
-import yaml
+import windowsapps
 # 2.Then, to search something, there are two ways: on YouTube or DuckDuckGo, we will use a function to do it
 def search(a):
-    # 2.1.First, if the user pass an order to search on YouTube, we will check it, because "pesquisar" is in "pesquisar no youtube"
-    if 'pesquisar no youtube' in a:
-        conteudo = a[20:]
-        os.startfile('https://www.youtube.com/results?search_query='+conteudo)
-        print('Pesquisando por '+conteudo)
-    # 2.2.If the last interaction doesn't occur, it will lead to this, when it will search everything that it's after "pesquisar"
-    elif 'pesquisar' in a:
-        a = a[9:]
-        os.startfile('https://duckduckgo.com/?q='+a)
-        print('Pesquisando por '+a)
+    a = a.split(' ')
+    if a[0].lower() == 'pesquisar':
+        if 'youtube' in a:
+            a = ' '.join(a[int(a.index('youtube'))+1:])
+            os.startfile('https://www.youtube.com/results?search_query='+a)
+        else:
+            a = ' '.join(a[int(a.index('pesquisar'))+1:])
+            os.startfile('https://www.google.com/search?q='+a)
+    else:
+        return
 # 3.Other thing that we can do it's open something, that will use a database that we made with searchapp.py
 def open(a):
-    data = yaml.safe_load(open('path.yml','r',encoding='utf-8').read())
-    for Aplicativo in data['Aplicativos']:
-        b = Aplicativo['Nome']
-        c = Aplicativo['Caminho']
-        if b in a:
-            os.startfile(c)
-            print('Executando '+b)
+    a = a.split(' ')
+    if a[0].lower() in ['abrir', 'abra', 'abre']:
+        a = a[1]
+    else:
+        a = a[0]
+    try:
+        print(a)
+        windowsapps.open_app(a)
+    except:
+        return
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 # 1.We will import some modules, keyboard was for a future use, but it's descontinued for now
 #import keyboard
 import funçoes
-import searchapp
 # Extra.Just the version, when I write it, it was on alpha 6.1
-print('§§§§§LAB a6.1§§§§§')
+print('§§§§§LAB a6.2§§§§§')
 # 2.A loop function to make it run while the user wants
 while True:
     # 3.The magic it's here, the prompt, the command, the input
@@ -11,10 +10,8 @@ while True:
     # 4. If you don't want to play, there is the exit door
     if 'sair' in a:
         break
-    # 5. If what you want it's to add something on the database, there is the way
-    elif 'adicionar' in a:
-        searchapp.adicionar()
     # 6.else, there is the real work that we saw in funçoes.py
     else:
-        funçoes.open(a)
         funçoes.search(a)
+        funçoes.open(a)
+        
